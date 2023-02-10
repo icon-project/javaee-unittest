10c6c06d8ca627155ce1d37407ada8a60133fabb
MoonKyu Song <mksong@iconloop.com>
2023-02-10 14:21:40 +0900

    Support inter-call simulation (#22)

--- unittest/src/main/java/score/ServiceManagerImpl.java
+++ unittest/src/main/java/score/ServiceManagerImpl.java
@@ -244,6 +244,15 @@ class ServiceManagerImpl extends ServiceManager implements AnyDBImpl.ValueStore 
         }
     }
 
+    @Override
+    public Object call(Account from, BigInteger value, Address targetAddress, String method, Object... params) {
+        if (from == null) {
+            throw new NullPointerException("from is null");
+        }
+        try (var scope = setupTransactionInfo(true)) {
+            return handleCall(from, value, true, false, targetAddress, method, params);
+        }
+    }
 
     @Override
     public Object call(Address targetAddress, String method, Object... params) {
